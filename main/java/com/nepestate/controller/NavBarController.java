38bd01fb4dfbc12384ed8d98833a53ac02f2a72f
Sansraya <pradhanag.sansraya@gmail.com>
2025-04-30 23:32:11 +0545

    Renamed files in NavBarController

--- main/java/com/nepestate/controller/NavBarController.java
+++ main/java/com/nepestate/controller/NavBarController.java
@@ -27,7 +27,7 @@ public class NavBarController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/pages/navbar.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/pages/Navbar.jsp").forward(request, response);
 	}
 
 	/**
