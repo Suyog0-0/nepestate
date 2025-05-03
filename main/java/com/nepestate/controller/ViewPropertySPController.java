2a0458c87677c17e3addfae6bad3ca5f4faff1ce
Suyog0-0 <suyogsigdel000@gmail.com>
2025-05-03 22:38:10 +0545

    Test Commit From terminal

--- main/java/com/nepestate/controller/ViewPropertySPController.java
+++ main/java/com/nepestate/controller/ViewPropertySPController.java
@@ -22,6 +22,7 @@ public class ViewPropertySPController extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
